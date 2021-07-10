15df48353390f71e1eb7752a26a34b49a826962a
Rushan <rrsufiyanov@gmail.com>
2021-07-10 12:08:54 +0300

    Add task 2

---
+++ Вычисление/task2.cs
@@ -0,0 +1,22 @@
+namespace ConsoleApp1
+{
+    class task2
+    {
+        static void Main()
+        {
+            /*Вывести на экран прямоугольник, заполненный буквами А. 
+              Количество строк в прямоугольнике равно 5, количество столбцов равно 8.
+            */
+            for (int i = 0; i < 6; i++) 
+            {
+                for (int s = 0; s < 8; s++)
+                {
+                    Console.Write("A ");
+                }
+                Console.WriteLine();
+
+            } 
+            Console.ReadKey();
+        }
+    }
+}
