eb448bf6cedec3d106c2498dc800da873b572edd
Rushan <rrsufiyanov@gmail.com>
2021-07-10 13:13:04 +0300

    add task 3

---
+++ Вычисление/task3.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class task3
+    {
+        static void Main()
+        {
+            //Вывести на экран букву "W" из символов "*" (звездочка).
+
+            Console.WriteLine("*   *   *");
+            Console.WriteLine(" * * * *");
+            Console.WriteLine("  *   *");
+            Console.ReadKey();
+        }
+    }
+}
