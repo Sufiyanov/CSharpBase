1b5c0b67aecfe5b9d4e0921fe82f43c006f368a2
Rushan <rrsufiyanov@gmail.com>
2021-07-15 22:48:44 +0300

    add

---
+++ Вычисление/task10.cs
@@ -0,0 +1,30 @@
+using System;
+using System;
+
+namespace ConsoleApp1
+{
+    class task9
+    {
+        static void Main()
+        {
+            /*Начав тренировки, лыжник в первый день пробежал 10 км. 
+              Каждый следующий день он увеличивал пробег на 10% от пробега предыдущего дня. 
+              Определите: а) пробег лыжника за второй, третий, ..., десятый день тренировок; 
+              б) какой суммарный путь он пробежал за первые 7 дней тренировок. 
+            */
+            double mileage = 10;
+            double totalPath = mileage; 
+            int i = 0;
+            while (i < 23)
+            {
+                mileage = mileage + mileage * 0.1;
+                Console.WriteLine("День " + i + " | Пробежал " + mileage);
+                if (i > 7) totalPath = totalPath + mileage;
+
+               i++;
+            }
+            Console.WriteLine("Сумарнный путь за 7 дней " + totalPath + " km");
+            Console.ReadKey();
+        }
+    }
+}
