00a65b9cfec55eab3a09415561ac29ccc8ca4c39
rushan <rushan@example.com>
2021-08-15 22:43:08 +0300

    add task 16

---
+++ Вычисление/task16.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class task16
+    {
+        static void Main()
+        {
+            // Найти наибольший элемент массива.
+
+            int[,,] mas = { { { 1, 2 },{ 3, 4 } },
+                { { 4, 5 }, { 6, 7 } },
+                { { 7, 8 }, { 9, 10 } },
+                { { 10, 11 }, { 12, 13 } }
+              };
+
+            int result = mas[0, 0, 0];
+            foreach (int i in mas)
+            {
+                if (result < i) result = i;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
